3629e7a1350ba86fdf93a99c5fedb9d09715229a
Aditya Singh <72691999+as1605@users.noreply.github.com>
2021-03-22 12:37:46 +0530

    Update q4.c

--- Lab2/q4.c
+++ Lab2/q4.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <math.h>
-void main()
-{
-    int x,n,i,power=1;
-    scanf("%d %d",&x,&n);
-    for(i=0;i<n;i++)
-    {
-       power=power*x;
-	    }
-     printf("%d",power);
-}
 
+void main() {
+    int x, n, power=1;
+    scanf("%d %d",&x,&n);
+    for(int i=0; i<n; i++) {
+    	power*=x;
+    }
+    printf("%d\n",power);
+}
