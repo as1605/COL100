3e08f74fe6ffdeb6afa184cefbdddd9a5243335f
Aditya Singh <72691999+as1605@users.noreply.github.com>
2021-03-22 12:32:54 +0530

    formatting

--- Lab2/q3.c
+++ Lab2/q3.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-void main()
-{
+
+void main() {
     int n;
     scanf("%d",&n);
-    if(n<2){
-             printf("NOT a Prime Number\n");
-    return;}
-    int i;
-    for(i=2;i*i<=n;i++) {
-          if(n==(n/i)*i)//fast way to check n%i==0
-	  {
-             printf("NOT a Prime Number\n");
-	     return;} }
-    printf("Is a Prime Number\n"); }
-
+    if (n<2) {
+        printf("NOT a Prime Number\n");
+    	return;
+    }
+    for(int i=2; i*i<=n; i++) {
+          if ( n==(n/i)*i ) { //fast way to check n%i==0
+          	printf("NOT a Prime Number\n");
+	  	return;
+	  } 
+    }
+    printf("Is a Prime Number\n"); 
+}
