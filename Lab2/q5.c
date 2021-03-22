492d8ccdfa4cb111a93395cc2d39e5ef15d32e36
Aditya Singh <72691999+as1605@users.noreply.github.com>
2021-03-22 12:54:25 +0530

    Update q5.c

--- Lab2/q5.c
+++ Lab2/q5.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
-void main()
-{
-  char choice;
- float pi=3.14159265;
-  scanf("%c",&choice);
-   float r,l,w,b,h;
-  switch(choice)
-  {
-       case 'C':
-	       scanf("%f",&r);
-	       printf("The area is %.2f",pi*r*r);
-	       break;
-      case 'R':
-	       scanf("%f %f",&l,&w);
-	       printf(" The area is %.2f",l*w);
-	       break;
-      case 'T':
-                scanf("%f %f",&b,&h);
-               printf("The area is %.2f",b*h/2);
-               break;	       
 
-      default:
-	       printf("Enter valid Input");
-  }
+void main() {
+	char choice;
+	float pi=3.14159265;
+	scanf("%c",&choice);
+	switch (choice) {
+    	case 'C':
+			float r;
+	       	scanf("%f",&r);
+	       	printf("The area is %.2f\n",pi*r*r);
+	       	break;
+      	case 'R':
+			float l,w;
+	      	scanf("%f %f",&l,&w);
+	      	printf(" The area is %.2f\n",l*w);
+	       	break;
+      	case 'T':
+			float b,h;
+           	scanf("%f %f",&b,&h);
+            printf("The area is %.2f\n",(b*h)/2);
+           	break;	       
+      	default:
+	     	printf("Invalid Input!\n");
+  	}
 }
