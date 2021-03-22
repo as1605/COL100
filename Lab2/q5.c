1af3621d47f529fdd8c9de978902d7d0ac7548f5
Aditya Singh <72691999+as1605@users.noreply.github.com>
2021-03-22 12:59:22 +0530

    Update q5.c

--- Lab2/q5.c
+++ Lab2/q5.c
@@ -4,19 +4,17 @@ void main() {
 	char choice;
 	float pi=3.14159265;
 	scanf("%c",&choice);
+	float r,l,w,b,h;
 	switch (choice) {
     	case 'C':
-			float r;
 	       	scanf("%f",&r);
 	       	printf("The area is %.2f\n",pi*r*r);
 	       	break;
       	case 'R':
-			float l,w;
 	      	scanf("%f %f",&l,&w);
 	      	printf(" The area is %.2f\n",l*w);
 	       	break;
       	case 'T':
-			float b,h;
            	scanf("%f %f",&b,&h);
             printf("The area is %.2f\n",(b*h)/2);
            	break;	       
