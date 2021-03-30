83717deb1b4e2c75a05809c55d1e265b21720432
as1605 <1605.aditya.singh@gmail.com>
2021-03-30 17:50:21 +0530

    Edited cos x question, zipped for final submission

--- Lab3/q4.c
+++ Lab3/q4.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-void main()
-{
-     float x,pi=3.14;//x is in degrees
-     scanf("%f",&x);
-     if(x>=0 && x<=180) {
-         float cosx=1,term=1,rad=pi*x/180;//x is input in degrees ,so converting it to radians
-         for(int i=2;i<=12;i+=2)
-        {
-           term*=(rad*rad)/(i*(i-1));
-            if(i%4==0)
-            {cosx+=term;}
-            else{cosx-=term;}
-         }
-         printf("%.3f",cosx);}
-     }
+
+int main() {
+	float x,pi=3.14159265359;//x is in degrees
+	scanf("%f",&x);
+	if(x>=0 && x<=180) {
+    	float cosx=1,term=1,rad=pi*x/180;//x is input in degrees ,so converting it to radians
+		for (int i=2;i<=12;i+=2) {
+    	    term*=(rad*rad)/(i*(i-1));
+    	    if(i%4==0) {
+    	    	cosx+=term;
+    	    } else{
+    	    	cosx-=term;
+    	    }
+    	}
+    	printf("%.3f\n",cosx);
+    }
+    return 0;
+}
 
 
 
