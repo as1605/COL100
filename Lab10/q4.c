baaf712b010ada549240bf3f1e1124862ca6cc41
Simran Malik <80638890+maliksimran2604@users.noreply.github.com>
2021-06-01 21:35:44 +0530

    Update q4.c

--- Lab10/q4.c
+++ Lab10/q4.c
@@ -1,12 +1,7 @@
 #include<stdio.h>
 int const size=100;
 int ternarysearch(int A[],int p,int q,int x) {
-        if(p==q) {
-        if(A[p]==x)
-                return 1;
-        else
-                return -1;
-        }
+        if(p<=q) {
     int mid1 = (2*p+q)/3;
     int mid2 = (p+2*q)/3;
     if(A[mid1]==x)
@@ -15,14 +10,31 @@ int ternarysearch(int A[],int p,int q,int x) {
 	    return 1;
 
     else {
-      if(x<A[mid1])
-              return ternarysearch (A,p,mid1-1,x);
-      else if(x<A[mid2])
-              return ternarysearch(A,mid1+1,mid2-1,x);
-      else
-	      return ternarysearch(A,mid2+1,q,x);
-    }
-
+      if(x<A[mid1]){
+	     int B[size];
+	      for(int i=0;i<mid1-p;i++){
+		 B[i]=A[i+p];      
+	      }
+              return ternarysearch (B,0,mid1-p-1,x);
+      }
+      else if(x<A[mid2]){
+	      int B[size];
+	      for(int i=0;i<mid2-mid1-1;i++){
+		 B[i]=A[i+mid1+1];      
+	      }
+              return ternarysearch(B,0,mid2-mid1-2,x);
+      }
+	      else  {
+	      	 int B[size];
+	      for(int i=0;i<q-mid2;i++){
+		 B[i]=A[i+mid2+1];      
+	      }     
+	      return ternarysearch(B,0,q-mid2-1,x);
+	      }
+     }
+  }
+    else
+	    return -1;
 }
 int main() {
    int n;
